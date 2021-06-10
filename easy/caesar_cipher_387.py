405440359265438f5e65203b66e4f08febd68499
VICamaraPrg <VICamaraPrg@outlook.com>
2021-06-10 19:21:52 +0200

    Added #387 Challenge + Bonus 1.

---
+++ easy/caesar_cipher_387.py
@@ -0,0 +1,32 @@
+"""
+Challenge #387 [Easy] Caesar cipher
+"""
+
+import string
+
+lower_alphabet = string.ascii_lowercase
+upper_alphabet = string.ascii_uppercase
+special_chars = (' ', ',', '.', '-',
+                 '_', '/', '\\', '\'',
+                 '\"', '?', '¿', '!',
+                 '¡', '@', '$', '%')
+
+
+# Challenge + Bonus 1
+def caesar(secret, steps):
+    ciphered = ''
+    for letter in secret:
+        if letter in special_chars:
+            ciphered += letter
+            continue
+        elif letter.islower():
+            idx = lower_alphabet.find(letter)
+            ciphered += lower_alphabet[(idx + steps) % 26]
+        else:
+            idx = upper_alphabet.find(letter)
+            ciphered += upper_alphabet[(idx + steps) % 26]
+
+    return ciphered
+
+
+print(caesar("jgorevxumxgsskx", 20))
